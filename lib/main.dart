e8be499337ecee57f25b0d3fb9225d22410c7a9d
Nana Kweku Otoo <77609447+NanasArsenal@users.noreply.github.com>
2021-01-18 22:03:18 +0000

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(title: Text('AppBar'),centerTitle: true,backgroundColor: Colors.blueGrey[900],),
+    ),
+  ));
+}
